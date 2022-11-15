ac2ae32903bcd0c74a78e7398b366bb0d0fd9ad0
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-11-15 14:32:03 +0500

    change color of boxes through declerative method

--- src/Box.js
+++ src/Box.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Box(props) {
   const styles = {
-    backgroundColor: props.on ? '#222222' : 'none',
+    backgroundColor: props.on ? '#222222' : 'transparent',
   }
 
-  return <div style={styles} className="box"></div>
+  return (
+    <div
+      style={styles}
+      className="box"
+      //   onClick={() => props.toggle(props.id)}
+      onClick={props.toggle}
+    ></div>
+  )
 }
